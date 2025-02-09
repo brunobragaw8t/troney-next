684c3e0c6c9b744c68837a64be5b052470fcbc84
Bruno Braga <brunobragaw8t@gmail.com>
2025-02-09 17:38:17 +0000

    feat(alert): create component

---
+++ src/app/_components/icons/IconICircle.tsx
@@ -0,0 +1,22 @@
+type Props = { className: string };
+
+export default function IconICircle(props: Props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={props.className}
+    >
+      <circle cx="12" cy="12" r="10"></circle>
+      <path d="M12 16v-4"></path>
+      <path d="M12 8h.01"></path>
+    </svg>
+  );
+}
---
+++ src/app/_components/icons/IconXCircle.tsx
@@ -0,0 +1,22 @@
+type Props = { className: string };
+
+export default function IconXCircle(props: Props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={props.className}
+    >
+      <circle cx="12" cy="12" r="10"></circle>
+      <path d="m15 9-6 6"></path>
+      <path d="m9 9 6 6"></path>
+    </svg>
+  );
+}
