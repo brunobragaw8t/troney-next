1926250c80babbd57b8661d5e85b2628c941ea8f
Bruno Braga <brunobragaw8t@gmail.com>
2025-02-10 20:55:33 +0000

    feat(button): add loading state

--- src/app/_components/Button.tsx
+++ src/app/_components/Button.tsx
@@ -1,13 +1,15 @@
 type ButtonProps = {
   type: "button" | "submit";
   label: string;
+  loading?: boolean;
 };
 
 export default function Button(props: ButtonProps) {
   return (
     <button
       type={props.type}
-      className="text-secondary-1 hover:bg-primary-2 bg-primary-1 rounded-lg border border-transparent p-2 outline-none focus:border-white"
+      className={`text-secondary-1 hover:bg-primary-2 bg-primary-1 rounded-lg border border-transparent p-2 outline-none focus:border-white ${props.loading ? "cursor-wait opacity-50" : ""}`}
+      disabled={props.loading}
     >
       {props.label}
     </button>
