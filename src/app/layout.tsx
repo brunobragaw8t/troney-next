a5c2a2a7b9813e1c154fe4fdd20efd2167bbf8a5
Bruno Braga <brunobragaw8t@gmail.com>
2025-02-11 21:33:29 +0000

    refactor(home): move markup to layout and AuthForms to Home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import type { Metadata } from "next";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import IconTroney from "./_components/icons/IconTroney";
 
 export const metadata: Metadata = {
   title: "Troney",
@@ -17,7 +18,24 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body className="bg-secondary-1">
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <TRPCReactProvider>
+          <main className="flex min-h-screen items-center justify-center">
+            <div className="flex w-96 flex-col gap-8 rounded-xl bg-secondary-2 p-8">
+              <div className="flex flex-col gap-4">
+                <div className="flex items-center justify-center gap-2 text-2xl font-semibold text-white">
+                  <IconTroney />
+                  <h1>Troney</h1>
+                </div>
+
+                <span className="text-center font-medium text-secondary-4">
+                  Track your expenses with ease
+                </span>
+              </div>
+
+              {children}
+            </div>
+          </main>
+        </TRPCReactProvider>
       </body>
     </html>
   );
