90d67b040ed1e46eaf65c31654d98622e3b2a98d
Bruno Braga <brunobragaw8t@gmail.com>
2025-02-18 21:30:43 +0000

    feat(auth): add button to return to login on activation page

--- src/app/_components/UserActivation.tsx
+++ src/app/_components/UserActivation.tsx
@@ -1,5 +1,6 @@
 import { api } from "~/trpc/server";
 import Alert from "./Alert";
+import Button from "./Button";
 
 type Props = {
   tokenValue: string;
@@ -10,17 +11,25 @@ export default async function UserActivation(props: Props) {
     await api.users.activate(props.tokenValue);
   } catch {
     return (
-      <Alert
-        type="error"
-        message="Your activation token is invalid or has expired."
-      />
+      <>
+        <Alert
+          type="error"
+          message="Your activation token is invalid or has expired."
+        />
+
+        <Button type="link" href="/" label="Return to login" />
+      </>
     );
   }
 
   return (
-    <Alert
-      type="success"
-      message="Your account has been activated successfully!"
-    />
+    <>
+      <Alert
+        type="success"
+        message="Your account has been activated successfully!"
+      />
+
+      <Button type="link" href="/" label="Return to login" />
+    </>
   );
 }
