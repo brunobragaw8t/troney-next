ffbe9ff98e57aed4c85af5cb5f781afdee784854
Bruno Braga <brunobragaw8t@gmail.com>
2025-02-19 20:05:20 +0000

    feat(security): prevent clickjacking by protecting against embedding

--- next.config.js
+++ next.config.js
@@ -5,6 +5,20 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default config;
