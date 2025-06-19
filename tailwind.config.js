94f32454da5239580e96534722f2f25df5a39232
Bruno Braga <brunobragaw8t@gmail.com>
2025-06-19 18:04:49 +0100

    feat(styles): add color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-1": "#2dd4bf",
+        "primary-2": "#4cedd9",
+        "secondary-1": "#0f172a",
+        "secondary-2": "#1e293b",
+        "secondary-3": "#334155",
+        "secondary-4": "#94a3b8",
+      },
+    },
   },
   plugins: [],
 };
