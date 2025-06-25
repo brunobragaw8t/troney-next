9571ce781bfa041e54be0f0d52274dfd71f90598
Bruno Braga <brunobragaw8t@gmail.com>
2025-06-25 21:54:59 +0100

    chore(db): setup Drizzle ORM and create Users table

---
+++ src/db/schema.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: varchar("name", { length: 256 }).notNull(),
+  email: varchar("email", { length: 256 }).notNull().unique(),
+  passwordHash: varchar("password_hash", { length: 256 }).notNull(),
+  activatedAt: timestamp("activated_at", { withTimezone: true }),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+});
