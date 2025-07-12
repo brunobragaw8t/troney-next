20b21043395ec0fa5e8de952e1e291b16db70c17
Bruno Braga <brunobragaw8t@gmail.com>
2025-07-12 16:01:50 +0100

    chore(trpc): setup tRPC with dummy route

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,14 @@
 import { AuthView } from "@/modules/auth/ui/views/auth-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-export default function Auth() {
-  return <AuthView />;
+export default async function Auth() {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "world" }));
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <AuthView />
+    </HydrationBoundary>
+  );
 }
