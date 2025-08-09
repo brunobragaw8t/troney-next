bd7ccd68a082f6da9cdafc0aab9565d1d65ad18a
Bruno Braga <brunobragaw8t@gmail.com>
2025-08-09 19:15:47 +0100

    feat(dashboard): add style to active menu item

--- src/app/(dasboard)/layout.tsx
+++ src/app/(dasboard)/layout.tsx
@@ -1,11 +1,11 @@
+"use client";
+
 import { Logo } from "@/components/brand/logo";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import {
   BarChart3,
   CreditCard,
-  PieChart,
-  Calendar,
-  Settings,
   TrendingUp,
   ArrowLeftRight,
   Wallet,
@@ -17,6 +17,8 @@ export default function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+
   const navItems = [
     {
       href: "/dashboard",
@@ -67,16 +69,15 @@ export default function DashboardLayout({
               <li key={item.href}>
                 <Link
                   href={item.href}
-                  className="group flex items-center gap-3 rounded-lg px-3 py-2.5 hover:bg-primary-2 hover:bg-opacity-10"
+                  className={`group flex items-center gap-3 rounded-lg px-3 py-2.5 text-secondary-4 hover:text-primary-1 ${
+                    pathname.startsWith(item.href)
+                      ? "bg-primary-2 bg-opacity-10 !text-primary-1"
+                      : ""
+                  }`}
                 >
-                  <item.icon
-                    size={20}
-                    className="text-secondary-4 group-hover:text-primary-1"
-                  />
+                  <item.icon size={20} />
 
-                  <span className="text-secondary-4 group-hover:text-primary-1">
-                    {item.label}
-                  </span>
+                  <span>{item.label}</span>
 
                   <span className="ml-auto min-w-5 rounded bg-secondary-3 px-1.5 py-0.5 text-center text-xs text-secondary-4 group-hover:text-primary-1">
                     {item.keymap}
