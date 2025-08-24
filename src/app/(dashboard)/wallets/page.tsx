f5bf5a4829782c6909fa4627429b343d90a1e41c
Bruno Braga <brunobragaw8t@gmail.com>
2025-08-24 08:34:38 +0100

    fix(wallets): make wallets page dynamic

--- src/app/(dashboard)/wallets/page.tsx
+++ src/app/(dashboard)/wallets/page.tsx
@@ -1,6 +1,8 @@
 import { WalletsView } from "@/modules/wallets/ui/views/wallets-view";
 import { HydrateClient, prefetch, trpc } from "@/trpc/server";
 
+export const dynamic = "force-dynamic";
+
 export default async function WalletsPage() {
   prefetch(trpc.wallets.getWallets.queryOptions());
 
