82a65ac8577fff8fff0184c7f3367da87e84c778
Bruno Braga <brunobragaw8t@gmail.com>
2025-10-08 20:13:34 +0100

    feat(buckets): add buckets CRUD procedures

---
+++ src/modules/buckets/server/procedures.ts
@@ -0,0 +1,122 @@
+import { db } from "@/db";
+import { buckets } from "@/db/schema";
+import {
+  baseProcedure,
+  createTRPCRouter,
+  protectedProcedure,
+} from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
+import { and, asc, eq, sql } from "drizzle-orm";
+import z from "zod";
+
+export const bucketsRouter = createTRPCRouter({
+  getBuckets: protectedProcedure.query(async ({ ctx }) => {
+    return await db
+      .select()
+      .from(buckets)
+      .where(eq(buckets.userId, ctx.session.userId))
+      .orderBy(asc(sql`LOWER(${buckets.name})`));
+  }),
+
+  getBucket: protectedProcedure
+    .input(z.string().uuid())
+    .query(async ({ ctx, input }) => {
+      const [bucket] = await db
+        .select()
+        .from(buckets)
+        .where(
+          and(eq(buckets.id, input), eq(buckets.userId, ctx.session.userId)),
+        );
+
+      if (!bucket) {
+        throw new TRPCError({
+          message: `Bucket ${input} not found`,
+          code: "NOT_FOUND",
+        });
+      }
+
+      return bucket;
+    }),
+
+  createBucket: protectedProcedure
+    .input(
+      z.object({
+        name: z
+          .string()
+          .min(1, "Name is required")
+          .max(255, "Name must be 255 characters or less")
+          .trim(),
+        budget: z.number().min(0, "Budget must be 0 or greater").default(0),
+        type: z.enum(["absolute", "percentage"]).default("absolute"),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const [bucket] = await db
+        .insert(buckets)
+        .values({
+          userId: ctx.session.userId,
+          name: input.name,
+          budget: input.budget.toString(),
+          type: input.type,
+        })
+        .returning();
+
+      return bucket;
+    }),
+
+  updateBucket: protectedProcedure
+    .input(
+      z.object({
+        id: z.string().uuid(),
+        name: z
+          .string()
+          .min(1, "Name is required")
+          .max(255, "Name must be 255 characters or less")
+          .trim(),
+        budget: z.number().min(0, "Budget must be 0 or greater"),
+        type: z.enum(["absolute", "percentage"]),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const [bucket] = await db
+        .update(buckets)
+        .set({
+          name: input.name,
+          budget: input.budget.toString(),
+          type: input.type,
+        })
+        .where(
+          and(eq(buckets.id, input.id), eq(buckets.userId, ctx.session.userId)),
+        )
+        .returning();
+
+      if (!bucket) {
+        throw new TRPCError({
+          message: `Bucket ${input.id} not found`,
+          code: "NOT_FOUND",
+        });
+      }
+
+      return bucket;
+    }),
+
+  deleteBucket: protectedProcedure
+    .input(z.string().uuid())
+    .mutation(async ({ ctx, input }) => {
+      const [bucket] = await db
+        .delete(buckets)
+        .where(
+          and(eq(buckets.id, input), eq(buckets.userId, ctx.session.userId)),
+        )
+        .returning();
+
+      if (!bucket) {
+        throw new TRPCError({
+          message: `Bucket ${input} not found`,
+          code: "NOT_FOUND",
+        });
+      }
+
+      return bucket;
+    }),
+});
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,11 +3,13 @@ import { authRouter } from "@/modules/auth/server/procedures";
 import { usersRouter } from "@/modules/users/server/procedures";
 import { walletsRouter } from "@/modules/wallets/server/procedures";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
+import { bucketsRouter } from "@/modules/buckets/server/procedures";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   users: usersRouter,
   wallets: walletsRouter,
+  buckets: bucketsRouter,
   categories: categoriesRouter,
 });
 
