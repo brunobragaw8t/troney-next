81df24da619d218b4148e796a0a8bd6bcb3e322b
Bruno Braga <brunobragaw8t@gmail.com>
2025-11-12 17:43:00 +0000

    fix(earnings): prefetch earnings list on the server

--- src/app/(app)/earnings/page.tsx
+++ src/app/(app)/earnings/page.tsx
@@ -1,9 +1,11 @@
 import { EarningsView } from "@/modules/earnings/ui/views/earnings-view";
-import { HydrateClient } from "@/trpc/server";
+import { HydrateClient, prefetch, trpc } from "@/trpc/server";
 
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
+  prefetch(trpc.earnings.getEarnings.queryOptions());
+
   return (
     <HydrateClient>
       <EarningsView />
