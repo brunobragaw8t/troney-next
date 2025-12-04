1b3147dce95cb351c8fd1d2f66a033f4bac68ba1
Bruno Braga <brunobragaw8t@gmail.com>
2025-12-04 10:10:32 +0000

    fix(movements): change source wallet name from header to cell in movements table

--- src/modules/movements/ui/sections/movements-table.tsx
+++ src/modules/movements/ui/sections/movements-table.tsx
@@ -104,9 +104,7 @@ export function MovementsTable() {
           {movements.map((movement, index) => (
             <TableRow key={movement.id} rowIndex={index} actions={actions}>
               <TableCell>{movement.date}</TableCell>
-              <TableHeader>
-                {movement.sourceWalletName || "Unknown"}
-              </TableHeader>
+              <TableCell>{movement.sourceWalletName || "Unknown"}</TableCell>
               <TableCell>{movement.value}</TableCell>
               <TableCell>{movement.targetWalletName || "Unknown"}</TableCell>
               <TableCell>
